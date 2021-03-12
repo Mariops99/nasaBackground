43cbc647fbf1d59047697c6afc2e25effdc77a7a
Mario <prietosanlesmario99@gmail.com>
2021-03-12 19:53:55 +0100

    Agregar archivos de proyecto.

---
+++ nasaBackgroundApp/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace nasaBackgroundApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.title2 = new System.Windows.Forms.Label();
+            this.setwallpaper = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.settings = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(105, 37);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(352, 37);
+            this.title.TabIndex = 0;
+            this.title.Text = "NASA Background GUI";
+            this.title.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // title2
+            // 
+            this.title2.AutoSize = true;
+            this.title2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title2.Location = new System.Drawing.Point(53, 123);
+            this.title2.Name = "title2";
+            this.title2.Size = new System.Drawing.Size(196, 25);
+            this.title2.TabIndex = 1;
+            this.title2.Text = "Select a specific day:";
+            this.title2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // setwallpaper
+            // 
+            this.setwallpaper.Location = new System.Drawing.Point(180, 271);
+            this.setwallpaper.Name = "setwallpaper";
+            this.setwallpaper.Size = new System.Drawing.Size(167, 50);
+            this.setwallpaper.TabIndex = 2;
+            this.setwallpaper.Text = "SET WALLPAPER";
+            this.setwallpaper.UseVisualStyleBackColor = true;
+            this.setwallpaper.Click += new System.EventHandler(this.setwallpaper_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(279, 126);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(108, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Automatic process";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // settings
+            // 
+            this.settings.Location = new System.Drawing.Point(342, 171);
+            this.settings.Name = "settings";
+            this.settings.Size = new System.Drawing.Size(75, 23);
+            this.settings.TabIndex = 5;
+            this.settings.Text = "ON";
+            this.settings.UseVisualStyleBackColor = true;
+            this.settings.Click += new System.EventHandler(this.settings_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 367);
+            this.Controls.Add(this.settings);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.setwallpaper);
+            this.Controls.Add(this.title2);
+            this.Controls.Add(this.title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label title2;
+        private System.Windows.Forms.Button setwallpaper;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button settings;
+    }
+}
+
---
+++ nasaBackgroundApp/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace nasaBackgroundApp
+{
+    public partial class Form1 : Form
+    {
+
+        RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+        private DateTime fecha;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fecha = dateTimePicker1.Value;
+            descargarImagen(fecha.Day, fecha.Month, fecha.Year);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void settings_Click(object sender, EventArgs e)
+        {
+            if (settings.Text == "ON")
+            {
+                settings.Text = "OFF";
+                rkApp.DeleteValue("nasaBackgroundApp", false);
+            }
+            else if (settings.Text == "OFF")
+            {
+                settings.Text = "ON";
+                rkApp.SetValue("nasaBackgroundApp", Application.ExecutablePath);
+                descargarImagen(fecha.Day, fecha.Month, fecha.Year);
+            }
+        }
+
+        private void setwallpaper_Click(object sender, EventArgs e)
+        {
+            descargarImagen(fecha.Day, fecha.Month, fecha.Year);
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            fecha = dateTimePicker1.Value;
+        }
+
+        private void descargarImagen(int dia, int mes, int año)
+        {
+            var url = $"https://api.nasa.gov/planetary/apod?api_key=dTdzuePtEEAMLfZ9EDehuxy2FOhbpMnEBFABwxyG&date=" + año + "-" + mes + "-" + dia;
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader == null) return;
+                        using (StreamReader objReader = new StreamReader(strReader))
+                        {
+                            string responseBody = objReader.ReadToEnd();
+                            var json = JsonConvert.DeserializeObject<dynamic>(responseBody);
+                            var imagenUrl = "";
+                            if (!json.ContainsKey("hdurl"))
+                            {
+                                if (!json.ContainsKey("url"))
+                                {
+                                    descargarImagen(15, 04, 2003);
+                                } else
+                                {
+                                    imagenUrl = json.url.ToString();
+                                }
+                            } else
+                            {
+                               imagenUrl = json.hdurl.ToString();
+                            }
+                            
+                            using (WebClient client = new WebClient())
+                            {
+                                client.DownloadFile(new Uri(imagenUrl), @"C:\nasaBackground\imageDay.png");
+                            }
+                        }
+                    }
+                }
+
+                // Sets the actual wallpaper
+                WinAPI.SystemParametersInfo(WinAPI.SPI_SETDESKWALLPAPER, 1, @"C:\nasaBackground\imageDay.png", WinAPI.SPIF_SENDCHANGE);
+
+            }
+            catch (WebException ex)
+            {
+                // Handle error
+                Console.WriteLine(ex);
+            }
+        }
+
+        public class WinAPI
+        {
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+            public const int SPI_SETDESKWALLPAPER = 20;
+            public const int SPIF_SENDCHANGE = 0x2;
+        }
+    }
+}
